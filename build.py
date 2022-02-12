236af44b89c2c80c65dca2898295d9c009195da8
Jonas Mucke <jonas.mucke@web.de>
2022-02-12 14:05:18 +0100

    Update build.py

--- build.py
+++ build.py
@@ -4,9 +4,9 @@ import subprocess
 
 # run cargo build --release
 # check if cargo build was successful
-if subprocess.run(["cargo", "build", "--release"]).returncode != 0:
-    print("cargo build failed")
-    sys.exit(1)
+#if subprocess.run(["cargo", "build", "--release"]).returncode != 0:
+#    print("cargo build failed")
+#    sys.exit(1)
 
 # move and rename ./target/release/package_name_rs.dll to ./package_name/package_name_rs.pyd
-os.rename("./target/release/package_name_rs.dll", "./package_name/package_name_rs.pyd")
+#os.rename("./target/release/package_name_rs.dll", "./package_name/package_name_rs.pyd")
